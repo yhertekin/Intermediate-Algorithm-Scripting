66e13367252aea275d205f6bd96b73bdd8c56aff
yhertekin <yhertekin@gmail.com>
2020-05-31 16:02:41 +0300

    Diff Two Arrays

---
+++ Diff Two Arrays/Diff Two Arrays.js
@@ -0,0 +1,10 @@
+function diffArray(arr1, arr2) {
+
+    return arr1
+    .filter(x => !arr2.includes(x))
+    .concat(arr2.filter(x => !arr1.includes(x)));
+    
+  }
+  
+  console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+  
