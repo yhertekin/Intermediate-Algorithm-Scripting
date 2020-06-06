d0601e957a8b7b93a82f7d5e05d81884ac9facd1
yhertekin <yhertekin@gmail.com>
2020-06-06 23:59:17 +0300

    Binary Agents

---
+++ Binary Agents/Binary Agents.js
@@ -0,0 +1,13 @@
+function binaryAgent(str) {
+    const coded = str.split(' ');
+    const decoded = [];
+  
+    coded.forEach(binary => {
+       decoded.push(String.fromCharCode(parseInt(binary, 2)));
+    });
+  
+    return decoded.join('');
+}
+  
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+  
---
+++ Binary Agents/README.md
@@ -0,0 +1,5 @@
+# Intermediate Algorithm Scripting: Binary Agents
+
+    Return an English translated sentence of the passed binary string.
+
+The binary string will be space separated.
